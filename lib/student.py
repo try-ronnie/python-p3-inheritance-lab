e343ce7169b85418fa1563c009e8353f64fd45d0
ronniegikandi <kiiruronnie42@gmil.com>
2025-11-29 01:36:18 +0300

    completed lab

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,13 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def __init__ (self, first_name , last_name):
+        super().__init__(first_name , last_name)
+        self.knowledge = []
+    
+    def learn(self , knowledge):
+        if isinstance(knowledge , str):
+            self.knowledge.append(knowledge)
+        else:
+            raise ValueError('the knowledge passed is not a string')
+    
